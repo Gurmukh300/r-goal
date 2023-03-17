f8aef99830257b267e71c9cbc99d73e1807cddfd
gurmukh singh <gurmukhs300@gmail.com>
2023-03-17 17:02:49 +1100

    css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,38 +1,39 @@
 import styled from 'styled-components';
-import './Button.css';
+import styles from'./Button.module.css';
+import React from 'react';
 
-const Button = styled.button`
-  width: 100%;
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  border: 1px solid #8b005d;
-  color: white;
-  background: #8b005d;
-  box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
-  cursor: pointer;
+// const Button = styled.button`
+//   width: 100%;
+//   font: inherit;
+//   padding: 0.5rem 1.5rem;
+//   border: 1px solid #8b005d;
+//   color: white;
+//   background: #8b005d;
+//   box-shadow: 0 0 4px rgba(0, 0, 0, 0.26);
+//   cursor: pointer;
   
-  @media (min-width: 768px) {
-    width: auto;
-  }
+//   @media (min-width: 768px) {
+//     width: auto;
+//   }
 
-  &:focus {
-    outline: none;
-  }
+//   &:focus {
+//     outline: none;
+//   }
 
-  &hover,
-  &active {
-    background: #2b0c20;
-    border-color: #25101e;
-    box-shadow: 0 0 8px rgba(230, 3, 3, 0.26);
-  }
-`;
+//   &hover,
+//   &active {
+//     background: #2b0c20;
+//     border-color: #25101e;
+//     box-shadow: 0 0 8px rgba(230, 3, 3, 0.26);
+//   }
+// `;
 
-// const Button = props => {
-//   return (
-//     <button type={props.type} className="button" onClick={props.onClick}>
-//       {props.children}
-//     </button>
-//   );
-// };
+const Button = props => {
+  return (
+    <button type={props.type} className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
